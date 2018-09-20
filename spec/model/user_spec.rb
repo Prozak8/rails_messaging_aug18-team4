38b94d57fce0aae5c60e76e515f67e1f20cb5491
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-20 09:39:39 +0200

    added unit tests for the user model

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    describe "db table" do
+        it { is_expected.to have_db_column :name }
+        it { is_expected.to have_db_column :email }
+        it { is_expected.to have_db_column :encrypted_password }
+    end
+end
