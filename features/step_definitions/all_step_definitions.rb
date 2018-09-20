e14a5235becc640192531998bd055940a7d966f6
Zak <zakary.howard94@gmail.com>
2018-09-20 14:56:47 +0200

    indent code for better readability, renamed variable sign_up to button

--- features/step_definitions/all_step_definitions.rb
+++ features/step_definitions/all_step_definitions.rb
@@ -1,16 +1,16 @@
 Given("I am on the landing page") do
-    visit root_path
-  end
+  visit root_path
+end
   
-  When("I press {string}") do |sign_up|
-    click_on sign_up
-  end
+When("I press {string}") do |button|
+  click_on button
+end
   
-  When("I fill in {string} with {string}") do |field, data|
-    fill_in field, with: data
-  end
+When("I fill in {string} with {string}") do |field, data|
+  fill_in field, with: data
+end
   
-  Then("I should see {string}") do |expected_content|
-    expect(page).to have_content expected_content
-  end
+Then("I should see {string}") do |expected_content|
+  expect(page).to have_content expected_content
+end
   
